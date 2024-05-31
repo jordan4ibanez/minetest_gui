dc95696c0e98e5587dab2ec43191cbb578d57765
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-31 02:06:05 -0400

    The button does something

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,16 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { trace, info, error, attachConsole } from "tauri-plugin-log-api";
 import { emit, listen } from '@tauri-apps/api/event';
 
-
+const random = Math.random;
 const detach = await attachConsole();
 
 const button: HTMLElement | null = document.getElementById("cool");
 
 if (button != null) {
   button.addEventListener("click", () => {
-    info("hi");
+    info("hi " + random());
+
+    button.innerText = "yep " + random();
   });
 } else {
   error("Button is null!");
@@ -20,12 +22,13 @@ if (button != null) {
 
 
 
+// The main loop which runs every 0.05 seconds.
 function onStep(): void {
-  info("hi");
+
 }
 
 
 
-// Internal timer runs main at 0.05 FPS.
+// Internal timer runs main at 20 FPS.
 setInterval(onStep, 50);
 detach();
