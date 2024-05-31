2e986b4b4ae0ff2e159161fe2e765d6f512a85a2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-31 04:42:00 -0400

    Tab buttons, almost work

--- src/library/buttonify.ts
+++ src/library/buttonify.ts
@@ -4,6 +4,10 @@ export { };
 
 // This is totally undocumented cause I'm a lazy fuck
 
+export enum Tabs {
+  environment, controls, settings
+}
+
 export function buttonClickEvent(buttonID: string, fun: () => void): void {
   const button: HTMLElement | null = document.getElementById(buttonID);
   if (button != null) {
@@ -13,3 +17,30 @@ export function buttonClickEvent(buttonID: string, fun: () => void): void {
   }
 }
 
+export function selectTab(tabID: string): void {
+  // Get all elements with class="tabcontent" and hide them
+  let tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    let element: HTMLElement = tabcontent[i] as HTMLElement;
+    element.style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  let tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  let currentTab = document.getElementById(tabID);
+
+  if (currentTab == null) {
+    error(`Tried to select tab ${tabID} which doesn't exist!`);
+    return;
+  }
+
+  // currentTab.style.display = "block";
+
+  currentTab.className += " active";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
-import { app, shell, tauri } from "@tauri-apps/api";
-import { invokeTauriCommand } from "@tauri-apps/api/helpers/tauri";
-import { Command } from "@tauri-apps/api/shell";
-import { invoke } from "@tauri-apps/api/tauri";
+// import { app, shell, tauri } from "@tauri-apps/api";
+// import { invokeTauriCommand } from "@tauri-apps/api/helpers/tauri";
+// import { Command } from "@tauri-apps/api/shell";
+// import { invoke } from "@tauri-apps/api/tauri";
 import { trace, info, error, attachConsole } from "tauri-plugin-log-api";
-import { emit, listen } from '@tauri-apps/api/event';
-import { buttonClickEvent } from "./library/buttonify";
+// import { emit, listen } from '@tauri-apps/api/event';
+import { buttonClickEvent, selectTab, Tabs } from "./library/buttonify";
 
-const random = Math.random;
+// const random = Math.random;
 const detach = await attachConsole();
 
-buttonClickEvent("environment", () => {
-  info("I am a button :D");
-});
+// Neaten this up. :)
+for (const tab of Object.keys(Tabs)) {
+  if (typeof Tabs[tab as unknown as Tabs] !== "string") {
+    const id = tab.toString();
+    buttonClickEvent(id, () => {
+      selectTab(id);
+    });
+  }
+}
 
 // The main loop which runs every 0.05 seconds.
 function onStep(): void {
