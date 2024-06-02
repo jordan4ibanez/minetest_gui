9833b21c365280cebbb9caaac422ea3b381528f3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-02 03:42:39 -0400

    I promise you <void>

--- src/library/settingify.ts
+++ src/library/settingify.ts
@@ -6,28 +6,30 @@ import { info } from "tauri-plugin-log-api";
 
 export { };
 
+const settingFileName: string = "settings.conf";
 // info(`Working in path: ${await appDataDir()}`);
 
 class Settingly {
   tab: Tabs = Tabs.environment;
 }
 
-export async function loadSettings() {
-  if (await exists("settings.conf", { dir: BaseDirectory.AppData })) {
+export async function loadSettings(): Promise<void> {
+  if (await exists(settingFileName, { dir: BaseDirectory.AppData })) {
     const text: string = await readTextFile("setting.conf", { dir: BaseDirectory.AppData });
     const thing: Object = JSON.parse(text);
 
     if (thing instanceof Settingly) {
-      
+      info("that's a thing :)");
+
     } else {
       info("STOP MODIFYING THE SETTINGS FILE WITHOUT READING THE SOURCE CODE PLEASE!");
     }
   }
 }
 
-export async function saveSettings() {
+export async function saveSettings(): Promise<void> {
   // info("writing thing");
-  await writeTextFile("settings.conf", JSON.stringify({ dir: "hi" }), { dir: BaseDirectory.AppData });
+  await writeTextFile(settingFileName, JSON.stringify({ dir: "hi" }), { dir: BaseDirectory.AppData });
   // await exists("poop.png", { dir: "" });
   // info("done");
 }
