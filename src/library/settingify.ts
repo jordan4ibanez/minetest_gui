7b34391cd8674c6fd988f17e7bca1e9a44ab46e7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-02 05:03:46 -0400

    Delete settingify.ts

--- src/library/settingify.ts
+++
@@ -1,7 +0,0 @@
-import { BaseDirectory, exists, readTextFile, writeTextFile } from "@tauri-apps/api/fs";
-import { Tabs } from "./buttonify";
-import { info } from "tauri-plugin-log-api";
-// import { appDataDir } from "@tauri-apps/api/path";
-// import { info } from "tauri-plugin-log-api";
-
-export { };
