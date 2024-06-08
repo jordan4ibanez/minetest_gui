3ade96844649031322b2acbd374f30baed90f9f8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-08 00:43:43 -0400

    Fix exports

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,4 +1,4 @@
-export { messageServer, serverPayload, startServer, updateServerRuntimeSettings } from "./server_server";
+export { triggerRestartWatch, killAllServers, restartWatch, messageServer, serverPayload, startServer, updateServerRuntimeSettings } from "./server_server";
 export { printf, Tabs, selectTab, tabify, safeGetElementByID, environmentTextAppend, buttonSettingsApply, safeAddEventListenerByID } from "./buttonify";
 export { tickTimeInMS, tickTimeInSeconds, Settings, loadSettings } from "./settings";
 export { loadCharts, addData, memoryPollLogic } from "./chartify";
