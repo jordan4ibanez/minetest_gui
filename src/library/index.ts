f6b8652452e88f469b23ab087b543ef6d283a72c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-08 03:14:54 -0400

    Update index.ts

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,4 +1,4 @@
-export { getPID, triggerRestartWatch, killAllServers, restartWatch, messageServer, serverPayload, startServer, updateServerRuntimeSettings } from "./server_server";
+export { isRunning, getPID, triggerRestartWatch, killAllServers, restartWatch, messageServer, serverPayload, startServer, updateServerRuntimeSettings } from "./server_server";
 export { printf, Tabs, selectTab, tabify, safeGetElementByID, environmentTextAppend, buttonSettingsApply, safeAddEventListenerByID } from "./buttonify";
 export { tickTimeInMS, tickTimeInSeconds, Settings, loadSettings } from "./settings";
 export { loadCharts, addData, memoryPollLogic } from "./chartify";
